7c0e689b833aa23f0aa59f92232e31e237b23158
ahmed0004321 <oasifrikto@gmail.com>
2025-09-20 09:10:20 +0600

    added files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+
+  return (
+    <>
+    <Countries></Countries>
+
+    <div>
+
+      <h1>React world on the go...</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
